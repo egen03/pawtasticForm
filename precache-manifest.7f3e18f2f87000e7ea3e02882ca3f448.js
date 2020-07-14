d5fad82854f08e1c7fd1f2ae4f73a31752f3368b
Student_GA111 <egen03@gmail.com>
2020-07-14 15:30:58 -0400

    Updates

---
+++ precache-manifest.7f3e18f2f87000e7ea3e02882ca3f448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6e4d180e1e54f7d28fff1a4aa16933",
+    "url": "/pawtawtasticform/index.html"
+  },
+  {
+    "revision": "f647dd68cb2434d7b8e7",
+    "url": "/pawtawtasticform/static/css/main.b5883269.chunk.css"
+  },
+  {
+    "revision": "cc1c87133100fe8fb26c",
+    "url": "/pawtawtasticform/static/js/2.15409837.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pawtawtasticform/static/js/2.15409837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f647dd68cb2434d7b8e7",
+    "url": "/pawtawtasticform/static/js/main.4582de77.chunk.js"
+  },
+  {
+    "revision": "e5ec368183fa855f7667",
+    "url": "/pawtawtasticform/static/js/runtime-main.2c5c1811.js"
+  }
+]);
